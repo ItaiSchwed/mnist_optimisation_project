bf4a2a1f2b7bb0258353f2d8a63e84908eb0305c
Itai Schwed <itai.schwed@gmail.com>
2020-01-19 08:14:18 +0200

    Roulette wheel 2 parents selection was added

--- genetic-algorithm/population.py
+++ genetic-algorithm/population.py
@@ -1,3 +1,5 @@
+import random
+
 import pandas as pd
 import numpy as np
 
@@ -12,13 +14,13 @@ class Population:
 
     def random_initialize_population(self):
         self.population = (pd.DataFrame(np.random.random((50, 5)))
-                           .apply(lambda row:
+                           .apply(lambda chromosome:
                                   pd.Series([
-                                      self.fitness(row[0]),
-                                      row[1] * 10,
-                                      row[2] * 10,
-                                      row[3] * 10,
-                                      row[4] * 10],
+                                      chromosome[0],
+                                      chromosome[1] * 10,
+                                      chromosome[2] * 10,
+                                      chromosome[3] * 10,
+                                      chromosome[4] * 10],
                                       index=['fitness_value',
                                              'budget',
                                              'leading_actor_count',
@@ -27,6 +29,8 @@ class Population:
                                   axis=1
                                   )
                            )
+        for index, row in self.population.iterrows():
+            self.population.loc[index, 'fitness_value'] = self.fitness(row)
         return
 
     def steady_state_replace(self, new_offsprings: pd.DataFrame):
@@ -53,16 +57,34 @@ class Population:
         else:
             self.involving_counter += 1
 
+    def choose_2_parents(self):
+        sum_of_finesses = (self.population
+                           .apply(lambda chromosome: chromosome['fitness_value'], axis=1)
+                           .sum())
+        return (self.__roulette_wheel_select_parent(sum_of_finesses),
+                self.__roulette_wheel_select_parent(sum_of_finesses))
+
+    def __roulette_wheel_select_parent(self, sum_of_finesses):
+        random_number = random.uniform(0, sum_of_finesses)
+        a = 7
+        for index, chromosome in self.population.iterrows():
+            random_number -= chromosome['fitness_value']
+            if random_number <= 0:
+                return chromosome
+        raise ValueError('Internal error')
+
     @staticmethod
     def fitness(off_spring: pd.Series):
-        return off_spring.loc['budget']
+        return 1 / off_spring.loc['budget']
 
 
 population = Population()
-new_offsprings = pd.DataFrame({
-    'fitness_value': [0.8, 0.6],
-    'budget': [20000, 30000],
-    'leading_actor_count': [2, 3],
-    'oscar_avg_per_actor': [0.1, 0.2],
-    'movie_length': [3000000, 300000]})
-population.steady_state_replace(new_offsprings)
+# new_offsprings = pd.DataFrame({
+#     'fitness_value': [1/8, 1/6],
+#     'budget': [8, 6],
+#     'leading_actor_count': [2, 3],
+#     'oscar_avg_per_actor': [0.1, 0.2],
+#     'movie_length': [3000000, 300000]})
+# population.steady_state_replace(new_offsprings)
+a = population.choose_2_parents()
+b = 7
